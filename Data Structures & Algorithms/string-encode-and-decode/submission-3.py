61e038100463d547b1f3e770cf27f855043b8f20
alastor <alcdev@icloud.com>
2026-08-04 15:29:18 -0700

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encodedRes = "";
+        for s in strs:
+            encodedRes = "".join([encodedRes, str(len(s)), "%", s])
+        return encodedRes
+
+    def decode(self, s: str) -> List[str]:
+        decodedStrs = [];
+        currIndex = 0;
+        print(s)
+        currStrLen = None;
+        while currIndex < len(s):
+            print("currIndex" + " " + str(currIndex))
+            print("currStrLen" + " " + str(currStrLen))
+            if (s[currIndex] == "%") & (currStrLen != None):
+                if currStrLen == "0":
+                    print("adding empty string")
+                    decodedStrs.append("")
+                else:
+                    decodedStrs.append(s[currIndex+1:int(currStrLen) + currIndex + 1])
+                currIndex += int(currStrLen) + 1
+                currStrLen = None
+            else:
+                if currStrLen:
+                    currStrLen = "".join([str(currStrLen), s[currIndex]])
+                else:
+                    currStrLen = s[currIndex]
+                currIndex += 1
+        return(decodedStrs)
